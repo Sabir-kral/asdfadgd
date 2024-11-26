1c238532d6eb8f652473204164f198d1c5cac187
Sabir-kral <sabirmemmedli21152014@gmail.com>
2024-11-26 14:22:16 +0400

    bruhho

---
+++ app.js
@@ -0,0 +1,39 @@
+let seher = prompt("azerbaycanin isgaldan azad olunan bir seher adini yaz onun hansi tarixde isgaldan azad olundugunu deyim")
+switch (seher) {
+case "lacin":
+        document.write("01.12.2020")
+break;
+case "cebrayil":
+        document.write("04.10.2020")
+break;
+case "zengilan":
+        document.write("20.10.2020")
+break;
+case "qubadli":
+        document.write("25.10.2020")
+break;
+case "susa":
+        document.write("08.11.2020")
+break;
+case "xankendi":
+        document.write("10.11.2020")
+break;
+case "xocali":
+        document.write("10.11.2020")
+break;
+case "xocavend":
+        document.write("10.11.2020")
+break;
+case "agdere":
+        document.write("10.11.2020")
+break;
+case "agdam":
+        document.write("20.11.2020")
+break;
+case "kelbecer":
+        document.write("25.11.2020")
+break;
+    default:
+        document.write(`${seher} azerbaycanda isgaldanazad olunmus bele bir seher yoxdu`)
+        break;
+}
